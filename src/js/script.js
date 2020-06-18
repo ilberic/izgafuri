ce42960e3e170554adbd6839fa83b38029c605da
Vadim <cltcmdc2001@gmail.com>
2020-06-18 16:05:20 +0500

    arrow scroll

--- src/js/script.js
+++ src/js/script.js
@@ -48,20 +48,23 @@ window.addEventListener('DOMContentLoaded', () => {
     'use strict';
     let tab = document.querySelectorAll('.portfolio__wrapper_item'),
         info = document.querySelector('.portfolio__wrapper'),
-        tabContent = document.querySelectorAll('.tabContent');
+        tabContent = document.querySelectorAll('.tabContent'),
+        sliderWrapper = document.querySelector('.slider__wrapper');
 
     function hideTabContent(a) {
         for (let i = a; i < tabContent.length; i++ ){
+            sliderWrapper.classList.add('hide');
             tabContent[i].classList.remove('show');
             tabContent[i].classList.add('hide');
             tabContent[i].classList.remove('anchor');
         }
     }
 
-    hideTabContent(1);
+    hideTabContent(0);
 
     function showTabContent(b) {
         if (tabContent[b].classList.contains('hide')){
+            sliderWrapper.classList.remove('hide');
             tabContent[b].classList.remove('hide');
             tabContent[b].classList.add('anchor');
             tabContent[b].classList.add('show');
@@ -75,8 +78,6 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     
-    anim(0);
-
     info.addEventListener('click',function(event){
         let target = event.target;
         if(target && target.classList.contains('portfolio__wrapper_item')){
@@ -109,40 +110,54 @@ window.addEventListener('DOMContentLoaded', () => {
         shadow.style.display = "none";
     });
     //block scroll
-    var anchor = [];
-    var currentAnchor = -1;
-    var isAnimating  = false;
-    $(function(){
-        function updateAnchors() {
-            anchor = [];
-            $('.anchor').each(function(i, element){
-                anchor.push( $(element).offset().top );
-            });
-        }
-        $('body').on('mousewheel', function(e){
-            e.preventDefault();
-            e.stopPropagation();
-            if( isAnimating ) {
-                return false;
-            }
-            isAnimating  = true;
-            if( e.originalEvent.wheelDelta >= 0 ) {
-                currentAnchor--;
-            }else{
-                currentAnchor++;
-            }
-            if( currentAnchor > (anchor.length - 1) 
-            || currentAnchor < 0 ) {
-                currentAnchor = 0;
-            }
-            isAnimating  = true;
-            $('html, body').animate({
-                scrollTop: parseInt( anchor[currentAnchor] )
-            }, 1000, 'swing', function(){
-                isAnimating  = false;
-            });
+    // var anchor = [];
+    // var currentAnchor = -1;
+    // var isAnimating  = false;
+    // $(function(){
+    //     function updateAnchors() {
+    //         anchor = [];
+    //         $('.anchor').each(function(i, element){
+    //             anchor.push( $(element).offset().top );
+    //         });
+    //     }
+    //     $('body').on('mousewheel', function(e){
+    //         e.preventDefault();
+    //         e.stopPropagation();
+    //         if( isAnimating ) {
+    //             return false;
+    //         }
+    //         isAnimating  = true;
+    //         if( e.originalEvent.wheelDelta >= 0 ) {
+    //             currentAnchor--;
+    //         }else{
+    //             currentAnchor++;
+    //         }
+    //         if( currentAnchor > (anchor.length - 1) 
+    //         || currentAnchor < 0 ) {
+    //             currentAnchor = 0;
+    //         }
+    //         isAnimating  = true;
+    //         $('html, body').animate({
+    //             scrollTop: parseInt( anchor[currentAnchor] )
+    //         }, 1000, 'swing', function(){
+    //             isAnimating  = false;
+    //         });
+    //     });
+    //     updateAnchors();   
+    // });
+        $('div').on('click', function() {
+          if ( $(this).hasClass('down') ) {
+            var movePos = $(window).scrollTop() + $(window).height();
+          }
+          if ( $(this).hasClass('top') ) {
+            var movePos = $(window).scrollTop() - $(window).height();
+          }
+          $('html, body').animate({
+            scrollTop: movePos}, 600);
         });
-        updateAnchors();   
-    });
-
+        $(window).scroll(function() {
+            var el = document.getElementsByTagName('body')[0];
+            el.style.overflow = 'hidden';
+            });
+            
 });
